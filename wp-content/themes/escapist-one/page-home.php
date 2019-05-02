2b25ef0ce7413d879b4c168acd149752f88a7545
hamishgray <hamish.gray@secretescapes.com>
2019-05-02 15:01:36 +0100

    custom home page. post groupings through shortcode

---
+++ wp-content/themes/escapist-one/page-home.php
@@ -0,0 +1,30 @@
+<?php
+/* Template Name: Home Page Template
+ *
+ * @package Escapist
+ */
+
+get_header(); ?>
+
+	<?php
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	?>
+
+	<div class="site-content-inner">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+		<?php get_sidebar(); ?>
+	</div><!-- .site-content-inner -->
+
+<?php get_footer(); ?>
