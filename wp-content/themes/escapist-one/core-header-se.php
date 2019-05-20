27b03508d63ca06ba40b1bdef3a29ad0cc1cb62b
hamishgray <hamish.gray@secretescapes.com>
2019-05-20 11:08:44 +0100

    Fix typo in SE core site header

--- wp-content/themes/escapist-one/core-header-se.php
+++ wp-content/themes/escapist-one/core-header-se.php
@@ -13,22 +13,22 @@
 
 				<ul class="core__nav">
 					<li class="core__nav-item">
-						<a href="https://www.secretescapes.se/aktuella-kampanjer" data-track="magazine-header-click" class="core__nav-link core__nav-link--orange">Aktuella Kampanjer</a>
+						<a href="https://www.secretescapes.se/aktuella-kampanjer" data-track="magazine-header-click" class="core__nav-link core__nav-link--orange">Aktuella kampanjer</a>
 					</li>
 					<li class="core__nav-item">
 						<a href="https://www.secretescapes.se/presentkort-offer" data-track="magazine-header-click" class="core__nav-link">Presentkort</a>
 					</li>
 					<li class="core__nav-item">
-						<a href="https://www.secretescapes.se/how-it-works" data-track="magazine-header-click" class="core__nav-link">Så Funkar Det</a>
+						<a href="https://www.secretescapes.se/how-it-works" data-track="magazine-header-click" class="core__nav-link">Så funkar det</a>
 					</li>
 				</ul>
 
 				<ul class="core__subnav">
 					<li class="core__subnav-item">
-						<a href="https://www.secretescapes.se/bjud-in-vanner" data-track="magazine-header-click" class="core__subnav-link">Bjud In Vänner</a>
+						<a href="https://www.secretescapes.se/bjud-in-vanner" data-track="magazine-header-click" class="core__subnav-link">Bjud in vänner</a>
 					</li>
 					<li class="core__subnav-item">
-						<a href="https://www.secretescapes.se/kontakt" data-track="magazine-header-click" class="core__subnav-link">Kontakta Oss</a>
+						<a href="https://www.secretescapes.se/kontakt" data-track="magazine-header-click" class="core__subnav-link">Kontakta oss</a>
 					</li>
 				</ul>
 
